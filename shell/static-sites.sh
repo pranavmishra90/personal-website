267f9bce96e807ac2a18ac6c1536935a02cf093c
Pranav Kumar Mishra <62562712+pranavmishra90@users.noreply.github.com>
2023-05-24 19:51:02 -0500

    updated shell script

--- shell/static-sites.sh
+++ shell/static-sites.sh
@@ -6,3 +6,9 @@ cd /home/pranav/personal-website/sites/prrx1-notebook
 git reset --hard
 git pull
 
+cd /home/pranav/personal-website
+
+git branch develop
+git commit ./sites -m 'Auto update of static sites'
+
+git push upstream develop
