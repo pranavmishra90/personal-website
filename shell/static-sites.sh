5c901d239da643d06c758b8014c857deac71467c
Pranav Kumar Mishra (Automated) <62562712+pranavmishra90@users.noreply.github.com>
2023-06-20 19:33:56 -0500

    Updated prrx1 site and crontab settings for git updates

--- shell/static-sites.sh
+++ shell/static-sites.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
+# Crontab settings
+# */20 * * * * bash /home/pranav/personal-website/shell/static-sites.sh >> /home/pranav/personal-website/shell/crontab.log 2>&1
+
 
 cd /home/pranav/personal-website/sites/prrx1-notebook
+echo "Directory is now" $PWD
 
+# Discard current changes in the static website
 git reset --hard
+
+# Pull down updates from GitHub
 git pull
 
+# Commit the changed submodule hash
 cd /home/pranav/personal-website
+echo "Directory is now" $PWD
 
-git branch develop
-git commit ./sites -m 'Auto update of static sites' -no-gpg-sign
+git checkout develop
+git commit ./sites -m 'Auto update of static sites'
 
 git push origin develop
+
+
+# Completed
+date
+echo "Done" 
