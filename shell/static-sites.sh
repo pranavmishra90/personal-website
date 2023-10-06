5f312931bdbe2a7cb43de2cb25a912b551cdc93b
Pranav Kumar Mishra <62562712+pranavmishra90@users.noreply.github.com>
2023-10-06 12:52:01 -0500

    May-Oct 2023 Update (#14)
    * Upgraded to PHP 8
    
    * removed SSL redirect, which is now deprecated
    
    * php8 not working, reverting to 7 temp
    
    * Added prrx1 notebook
    
    * updated shell script
    
    * removing requirement of signed commits in auto due to password requirement
    
    * changed traefik to latest
    
    * Updated networking
    
    * Auto update of static sites
    
    * Updated prrx1 site and crontab settings for git updates
    
    * Auto update of static sites
    
    * Auto update of static sites
    
    * Auto update of static sites
    
    * Auto update of static sites
    
    * Auto update of static sites
    
    * Auto update of static sites
    
    * Fix: Authelia config
    
    Why: Was not allowing authelia to boot
    
    How: Attempted using newer authelia tag --> container crashed,
    Then reverted back, added duo configuration parameters,
    using smtp for notification
    
    Tags: authelia
    
    * Auto update of static sites
    
    * Auto update of static sites
    
    * Auto update of static sites
    
    * Auto update of static sites
    
    * Auto update of static sites
    
    * Added BFGI notebook to Traefik, nginx, static sites
    
    * Auto update of static sites
    
    * Auto update of static sites
    
    * Auto update of static sites
    
    * Add: Monica PRM
    
    Why: Want to keep track of important people / events withfamily / friends
    
    How: Could not get this working with mounted volumes, but is working
    within docker volumes. Have a separate stack under ~/personal-website/monica
    
    Tags:
    
    * Fix: Changed directory permissions to get matomo working on new VPS
    
    Why: it wasn't working before
    
    How: chmod
    
    Tags:
    
    * Fix: removing permitted IPs from docker-compose
    
    * Updating static websites for the BFGI and Prrx1 experiments
    
    * Auto update of static sites
    
    * Auto update of static sites
    
    * Auto update of static sites
    
    * Auto update of static sites
    
    * Add: SterlingPDF docker container for PDF editing
    
    * Auto update of static sites
    
    * Auto update of static sites

--- shell/static-sites.sh
+++ shell/static-sites.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-# Crontab settings
-# */20 * * * * bash /home/pranav/personal-website/shell/static-sites.sh >> /home/pranav/personal-website/shell/crontab.log 2>&1
 
+echo "----------------------------"
+echo "Starting static site update"
+date
+echo "----------------------------"
+
+# Prepare environment
+
+source /home/pranav/personal-website/shell/bfgi.env
+
+# Check if SSH agent is running, if not, start a new one
+if [ -z "$SSH_AGENT_PID" ]; then
+    eval "$(ssh-agent -s)"
+fi
+
+ssh-add /home/pranav/.ssh/automated_ed25519
+
+# Prrx1
+#-------------------------------------------------------
 
 cd /home/pranav/personal-website/sites/prrx1-notebook
 echo "Directory is now" $PWD
@@ -13,16 +29,42 @@ git reset --hard
 # Pull down updates from GitHub
 git pull
 
-# Commit the changed submodule hash
-cd /home/pranav/personal-website
+
+# BFGI
+#-------------------------------------------------------
+cd /home/pranav/personal-website/sites/bfgi-notebook
 echo "Directory is now" $PWD
 
-git checkout develop
-git commit ./sites -m 'Auto update of static sites'
+# Discard current changes in the static website
+git reset --hard
+
+# Pull down updates from GitHub
+datalad update --how merge
+
+datalad get .
+
+datalad unlock .
+
+
+# Commit changes to DrPM ~/personal-website
+#-------------------------------------------------------
+
+# Commit the changed submodule hash
+cd /home/pranav/personal-website
+echo "Changing directory to the root: " $PWD
+
+# Change the signing key to automatic
+git config user.signingkey /home/pranav/.ssh/automated_ed25519.pub
+
+# git checkout develop
+git add ./sites
+git commit -a -m 'Auto update of static sites'
 
 git push origin develop
 
+# Change back the signing key to manual
+git config user.signingkey /home/pranav/.ssh/id_ed25519.pub
 
 # Completed
 date
-echo "Done" 
+echo "Done"
